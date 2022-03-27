e56fa071d7fe6606656403e6761268d1a69ed23d
jackcmason <jack.mason@posteo.net>
2022-03-27 18:08:05 +1000

    move bluetooth functions to bluetooth library, move functionality to threads

--- oslib/scu_drivers/scu_ble/scu_ble.h
+++ oslib/scu_drivers/scu_ble/scu_ble.h
@@ -10,5 +10,13 @@
 #ifndef SCU_BLE_H
 #define SCU_BLE_H
 
+/* Debug Thread Stack size */
+#define THREAD_BLE_BASE_STACK 4094
+
+/* Debug Thread Priority */
+#define THREAD_PRIORITY_BLE_BASE -2
+
+void thread_data(void);
+void thread_bluetooth();
 
 #endif //SCU_BLE_H
