aad0ecdac93293b151dddfd481b3c99f0d80eb5b
jackcmason <jack.mason@posteo.net>
2022-03-28 18:38:20 +1000

    power management functionality to library files

--- oslib/scu_drivers/scu_power_management/scu_power_management.c
+++ oslib/scu_drivers/scu_power_management/scu_power_management.c
@@ -7,3 +7,31 @@
   * ***************************************************************************
   */
 
+#include <pm/pm.h>
+#include <pm/device.h>
+#include <pm/device_runtime.h>
+#include <pm/state.h>
+
+void device_low_power(const struct device *dev1, 
+const struct device *dev2,
+const struct device *dev3,
+const struct device *dev4,
+const struct device *dev5) {
+  pm_device_action_run(dev1, PM_DEVICE_ACTION_SUSPEND);
+  pm_device_action_run(dev2, PM_DEVICE_ACTION_SUSPEND);
+  pm_device_action_run(dev3, PM_DEVICE_ACTION_SUSPEND);
+  pm_device_action_run(dev4, PM_DEVICE_ACTION_SUSPEND);
+  pm_device_action_run(dev5, PM_DEVICE_ACTION_SUSPEND);
+}
+
+void device_resume(const struct device *dev1, 
+const struct device *dev2,
+const struct device *dev3,
+const struct device *dev4,
+const struct device *dev5) {
+  pm_device_action_run(dev1, PM_DEVICE_ACTION_RESUME);
+  pm_device_action_run(dev2, PM_DEVICE_ACTION_RESUME);
+  pm_device_action_run(dev3, PM_DEVICE_ACTION_RESUME);
+  pm_device_action_run(dev4, PM_DEVICE_ACTION_RESUME);
+  pm_device_action_run(dev5, PM_DEVICE_ACTION_RESUME);
+}
--- oslib/scu_drivers/scu_power_management/scu_power_management.h
+++ oslib/scu_drivers/scu_power_management/scu_power_management.h
@@ -10,5 +10,16 @@
 #ifndef SCU_PM_H
 #define SCU_PM_H
 
+void device_resume(const struct device *dev1, 
+const struct device *dev2,
+const struct device *dev3,
+const struct device *dev4,
+const struct device *dev5);
+
+void device_low_power(const struct device *dev1, 
+const struct device *dev2,
+const struct device *dev3,
+const struct device *dev4,
+const struct device *dev5);
 
 #endif //SCU_PM_H
